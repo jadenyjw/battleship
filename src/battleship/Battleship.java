444f8877c30b8b6a4421710b6efd541a1297e3e9
yuting9 <yuting9@gmail.com>
2015-11-29 08:49:53 -0500

    Revert Imports
    I did something in the imports, and I'm trying to fix it.

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -3,10 +3,10 @@ package battleship;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
