d1a5a6d3ca78ce6b3f14b89e3ca73c2b3e0a617c
yuting9 <yuting9@gmail.com>
2015-11-29 08:46:54 -0500

    Fixed Image
    Made the image file readable from any computer (please test)

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -3,10 +3,10 @@ package battleship;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,6 +17,7 @@ import javax.swing.JPanel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.ImageIcon;
 import java.awt.FlowLayout;
+
 import javax.swing.BoxLayout;
 import javax.swing.JLayeredPane;
 import java.awt.Color;
@@ -117,7 +118,7 @@ public class Battleship {
   
   JLabel lblNewLabel = new JLabel("");
   lblNewLabel.setBounds(-92, 0, 620, 349);
-  lblNewLabel.setIcon(new ImageIcon("battleship\\src\\battleship\\img\\battleship.jpg"));
+  lblNewLabel.setIcon(new ImageIcon("img\\battleship.jpg"));
   layeredPane.add(lblNewLabel);
   
  }//End initMain
