37f9fe404c613a6323e94aec9cebfea08bc69103
jaden71 <jadenyjw@gmail.com>
2015-11-30 17:09:25 -0500

    lan discovery

--- src/battleship/Multimenu.java
+++ src/battleship/Multimenu.java
@@ -7,13 +7,23 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.esotericsoftware.kryonet.Client;
+
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+
 import java.awt.CardLayout;
 import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.util.List;
+import java.awt.event.ActionEvent;
 
 public class Multimenu extends JDialog {
 	private JPanel panel;
@@ -22,6 +32,8 @@ public class Multimenu extends JDialog {
 	private JPanel panel_1;
 	private JTextField txtName;
 	private JList list;
+	private JButton btnRefresh;
+	private JLabel lblEnterUsername;
 
 	/**
 	 * Launch the application.
@@ -40,13 +52,26 @@ public class Multimenu extends JDialog {
 	 * Create the dialog.
 	 */
 	public Multimenu() {
-		setBounds(100, 100, 237, 229);
+		setBounds(100, 100, 300, 300);
 		getContentPane().setLayout(new BorderLayout());
 		{
 			panel = new JPanel();
 			getContentPane().add(panel, BorderLayout.SOUTH);
 			{
+				btnRefresh = new JButton("Refresh");
+				btnRefresh.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+					}
+				});
+				panel.add(btnRefresh);
+			}
+			{
 				btnHostGame = new JButton("Host Game");
+				btnHostGame.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+
+					}
+				});
 				panel.add(btnHostGame);
 			}
 			{
@@ -59,16 +84,33 @@ public class Multimenu extends JDialog {
 			panel_1 = new JPanel();
 			getContentPane().add(panel_1, BorderLayout.NORTH);
 			{
+				lblEnterUsername = new JLabel("Enter Username:");
+				panel_1.add(lblEnterUsername);
+			}
+			{
 				txtName = new JTextField();
-				txtName.setText("Name");
+				txtName.setHorizontalAlignment(SwingConstants.LEFT);
 				panel_1.add(txtName);
 				txtName.setColumns(10);
 			}
 		}
 		{
+
 			list = new JList();
 			getContentPane().add(list, BorderLayout.CENTER);
+            refresh();
 		}
 	}
 
+	private void refresh() {
+		Client client = new Client();
+		List<InetAddress> address = client.discoverHosts(1337, 1337);
+		System.out.println(address);
+		DefaultListModel listModel;
+		listModel = new DefaultListModel();
+		for (int i = 0; i < address.size(); i++) {
+			listModel.addElement(address.get(i));
+		}
+        list.setModel(listModel);
+	}
 }
