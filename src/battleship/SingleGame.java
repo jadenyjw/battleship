8d0bd3709db12107b16e404c473636a12c059150
yuting9 <yuting9@gmail.com>
2016-01-11 10:03:10 -0500

    once more unto the breech

--- src/battleship/SingleGame.java
+++ src/battleship/SingleGame.java
@@ -481,7 +481,9 @@ public class SingleGame {
 		int choice;
 		for(int x = 0; x<10; x++){
 			for(int y = 0; y<10; y++){
-				enemyButtons[x][y].setIcon(GridButton.shipIcon);
+				if(enemyButtons[x][y].getDisabledIcon() == GridButton.hiddenShip){
+					enemyButtons[x][y].setDisabledIcon(GridButton.shipIcon);
+				}
 			}
 		}
 		
